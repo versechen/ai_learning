dfe1676798f0bb23168b763c3088298f5594df32
versechen <WeiCsir@outlook.com>
2025-12-11 20:17:14 +0800

    feat: mac init

--- book-0to1buildLLM/02-chapter/02-bep.py
+++ book-0to1buildLLM/02-chapter/02-bep.py
@@ -1,10 +1,12 @@
 from importlib.metadata import version
+
 import tiktoken
+
 print("tiktoken version:", version("tiktoken"))
 tokenizer = tiktoken.get_encoding("gpt2")
 text = (
     "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
-     "of someunknownPlace."
+    "of someunknownPlace."
 )
 integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
 print(integers)
