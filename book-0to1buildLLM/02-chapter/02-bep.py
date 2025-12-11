4dff269dfc9f56b1e764d0e4679665c50b6920e0
versechen <WeiCsir@outlook.com>
2025-12-11 07:54:27 +0000

    feat: bep

---
+++ book-0to1buildLLM/02-chapter/02-bep.py
@@ -0,0 +1,12 @@
+from importlib.metadata import version
+import tiktoken
+print("tiktoken version:", version("tiktoken"))
+tokenizer = tiktoken.get_encoding("gpt2")
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
+     "of someunknownPlace."
+)
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
+strings = tokenizer.decode(integers)
+print(strings)
